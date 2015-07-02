81c6a903fd04a9984edf046a6718bb0964aefcd0
Macbook Pro Andrew <annguyen@scarletmail.rutgers.edu>
2015-07-02 19:57:13 -0400

    Added comments in code

--- Reverse.java
+++ Reverse.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class Reverse {
-	public static void main(String args[]) {
+	public static void main(String args[]) {  // Main Method
 		String word = "Hello";
 		System.out.println(reverse2(word));
 	}
 	
-	public static String reverse1(String text) {
+	public static String reverse1(String text) { // Uses 1 for loop
 	 int wordlength = text.length();
 	for(int i = wordlength; i > 0; i--) {
 		text = text.concat(text.substring(i-1, i));
@@ -14,7 +14,7 @@ public class Reverse {
 	text = text.substring(wordlength, text.length());		
 	return text;
 	}
-	public static String reverse2(String text) {
+	public static String reverse2(String text) { // Uses recursion
 		if (text.length() == 0)
 			return text;
 		return reverse2(text.substring(1)) + text.charAt(0);
